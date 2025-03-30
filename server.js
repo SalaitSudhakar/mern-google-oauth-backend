ababb7e2e6294aa713471a2c884e0cf3e9c7a7a6
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-03-30 12:16:10 +0530

    create default api route

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './Database/config.js';
-
+import userRoute from './Routes/userRoute.js'
 dotenv.config();
 
 const app = express();
 
 const port =  process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my API")
+})
+
+app.use("/api/auth", userRoute);
+
 
 connectDb();
 
