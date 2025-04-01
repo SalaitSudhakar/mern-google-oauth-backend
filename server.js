afdf56f1d47cf1104faec46f359af8359ed27ef8
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-04-01 22:15:47 +0530

    Add front end deployed url to allowed origins for cors()

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = [process.env.FRONTEND_URL]
+const allowedOrigins = ["https://mern-google-auth.netlify.app"]
 
 app.use(express.json());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
