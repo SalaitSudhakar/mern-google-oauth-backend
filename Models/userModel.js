47061e893fe102d37e75d264c5c52cbc3d5c9dc5
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-04-01 17:27:55 +0530

    Update Default Image link in Model

--- Models/userModel.js
+++ Models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
     profile: {
       type: String,
       default:
-        "https://www.google.com/imgres?q=profile%20picture%20placeholder&imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F87%2F14%2F55%2F8714556a52021ba3a55c8e7a3547d28c.jpg&imgrefurl=https%3A%2F%2Fin.pinterest.com%2Fpin%2Fprofile-icon-png-image-free-download-searchpng-employee-photo-placeholder-transparent-png-is-free-tran--1020487596788968152%2F&docid=m_BKqgClh6NXXM&tbnid=tEgDKjy456xtdM&vet=12ahUKEwie6cqckrSMAxVeSGcHHVfjHIEQM3oECGgQAA..i&w=736&h=580&hcb=2&ved=2ahUKEwie6cqckrSMAxVeSGcHHVfjHIEQM3oECGgQAA",
+        "https://i.pinimg.com/736x/87/14/55/8714556a52021ba3a55c8e7a3547d28c.jpg",
     },
   },
   { timestamps: true }
